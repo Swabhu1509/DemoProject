34ab741fe4ae8cb597cabe5c3ea65edba65a47fd
Swabhu1509 <swabhaviwagh1509@gmail.com>
2022-09-13 18:14:58 +0530

    Code for ArrayList added

---
+++ ProjectVelocity/src/com/velocity/collection/ArrayListDemo.java
@@ -0,0 +1,23 @@
+package com.velocity.collection;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		
+	List<String> list = Arrays.asList("Prathmesh","Swabhavi","Rutuja","Harshu");
+	
+	Iterator<String> itr = list.iterator();
+	while(itr.hasNext()) {
+		
+		System.out.println(itr.next());
+	}
+
+
+	}
+
+}
